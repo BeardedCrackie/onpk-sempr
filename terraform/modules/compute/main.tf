28e96dbf449f09577fccf87961475c4296212836
MartinP1199 <potociarmartin@gmail.com>
2022-12-20 15:01:24 +0100

    main.tf code clean

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -21,10 +21,6 @@ resource "openstack_compute_instance_v2" "instance"{
   //image_id        = data.openstack_images_image_v2.image_id.id #Unable to query images: Get "http://controller:9292... : OpenStack connection error, retries exhausted.
   flavor_id      = data.openstack_compute_flavor_v2.flavor_id.id
   key_pair        = var.key_pair_name
-
-  #security_groups = [ "${openstack_networking_secgroup_v2.secgroup_01.id}" ]
-  #security_groups = [ var.security_group_id.name ]
-  #security_groups = [ openstack_networking_secgroup_v2.secgroup_01.name ]
   security_groups = var.secgroups
   user_data = var.user_data
 
