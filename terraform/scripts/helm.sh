4fe5c31c7e36193000e4b6e57b42b3bdea584bf4
MartinP1199 <potociarmartin@gmail.com>
2022-12-21 23:54:01 +0100

    helm.sh update to install helm packages at start

--- terraform/scripts/helm.sh
+++ terraform/scripts/helm.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 snap install helm --classic
+
+git clone https://github.com/MartinP1199/onpk-sempr.git
+
+cp -r onpk-sempr/helm/ . 
+cd appbackend/
+helm dependency build Chart.yaml
+helm install backend .
+
+cd ../appfrontend/
+helm dependency build Chart.yaml
+helm install frontend .
+
